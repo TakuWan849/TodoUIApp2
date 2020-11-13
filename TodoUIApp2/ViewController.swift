7d4a39b5c798d050f9ea17fc0fefc99f79a25ba7
TakuWan849 <wantaku339@gmail.com>
2020-11-14 04:24:24 +0900

    Initial Commit

---
+++ TodoUIApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoUIApp2
+//
+//  Created by 野澤拓己 on 2020/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
